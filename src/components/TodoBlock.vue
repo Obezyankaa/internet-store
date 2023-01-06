e4c19a2f8e4c70bf68c592e054d201896b7bb171
VladAir <vladair@MacBook-Air-VladAir.local>
2023-01-06 19:25:58 +0300

    add todos

--- src/components/TodoBlock.vue
+++ src/components/TodoBlock.vue
@@ -9,13 +9,24 @@ export default {
       id: 0,
     }
   },
+
+  async mounted() {
+    const data = await localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data);
+    }
+  },
+
   methods: {
     addTodo() {
-      this.todos.push({
-        id: this.id,
-        text: this.todo,
-        isComplete: this.isDone,
-      });
+      if (this.todo != '') {
+        this.todos.push({
+          id: this.id,
+          text: this.todo,
+          isComplete: this.isDone,
+        });
+        localStorage.setItem('todos', JSON.stringify(this.todos))
+      }
       this.todo = '';
     },
     // deleted(id) {
@@ -23,8 +34,13 @@ export default {
     // }
     removeTodo(id) {
       this.todos.splice(id, 1);
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+
         // this.todos = this.todos.filter(todo => todo.id !== id);
-    }
+    },
+    // isDone() {
+
+    // }
   }
 };
 </script>
@@ -35,23 +51,25 @@ export default {
   <!-- <form v-on:submit.prevent="addTodo"> -->
   <input v-model="todo" type="text" placeholder="Text..." />
   <button v-on:click="addTodo(id++)">add todo</button>
-  <div> Кол-во залач на сегодня {{ todos.length }} </div>
-  <!-- </form> -->
-<div v-for="(todo, index ) in todos" :key="todo.id">
-  <div class="todo__block">
-    <span class="todo__id">{{ index + 1 }}. </span>
-   <span class="todo__text" :class="{ todo__text_isShow: todo.isComplete}" @dblclick="removeTodo(index)"
->
-    {{ todo.text }}
-  </span> 
-   <input v-model="todo.isComplete" class="todo__check" type="checkbox">
-   <button v-on:click="deleted(todo.id)">удалить</button>
+  <div class="block__counter"> Кол-во залач на сегодня {{ todos.length }} </div>
+  <hr>
+  <div v-for="(todo, index ) in todos" :key="todo.id">
+    <div class="todo__block">
+      <span class="todo__id">{{ index + 1 }}. </span>
+      <span class="todo__text" :class="{ todo__text_isShow: todo.isComplete }" @dblclick="removeTodo(index)">
+        {{ todo.text }}
+      </span>
+      <input v-model="todo.isComplete" class="todo__check" type="checkbox">
+    </div>
   </div>
-</div>
+  <!-- </form> -->
 </div>
 </template>
 
 <style lang="scss">
+.block__counter {
+  padding: 5px 0px;
+}
 .todo {
   width: 300px;
   height: 100%;
